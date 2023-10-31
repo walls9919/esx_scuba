9390976b6a70b04efa2354341bccd27efa1e90dc
Walls <blumvaido@gmail.com>
2023-10-31 02:55:18 +0200

    Create et.lua
    Create estonian translation for the resource

---
+++ locales/et.lua
@@ -0,0 +1,14 @@
+Locales['et'] = { 
+    ['no_tank'] = 'Sul puudub sukeldumiskomplekt',
+    ['tank_loaded'] = 'Hapnikuballoon täidetud %s %s',
+    ['tank_capacity'] = 'Hapnikuballooni näidik %s %s',
+    ['tank_remaining'] = 'Hapnikuballoon kestab veel %s %s',
+    ['tank_available'] = 'Hapnikuballoon valmis kasutamiseks. Näidik: %s %s',
+    ['tank_not_available'] = 'Hapnikuballooni ei saa kasutada',
+    ['diving_fins'] = 'Sukeldumisuimedega sukeldumine',
+    ['diving_no_fins'] = 'Sukeldud ilma sukeldumisuimedeta',
+    ['push_refill'] = 'Vajuta [E], et täita hapnikuballooni',
+    ['push_refill_pay'] = 'Maksid hapnikuballooni täitmise eest: ',
+    ['no_money'] = 'Sul pole piisavalt raha',
+    ['not_equipped'] = 'Sul pole hapnikuballooni seljas'
+  }
